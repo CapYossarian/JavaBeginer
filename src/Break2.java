0b8e8c19365835c4a540b721072d638fb8511792
CapYossarian <litovka.sergey@gmail.com>
2023-02-13 22:10:28 +0300

    Initial commit

---
+++ src/Break2.java
@@ -0,0 +1,16 @@
+public class Break2 {
+    public static void main(String[] args) throws java.io.IOException {
+        char ch, ignore;
+        System.out.println("q-Выход");
+        for (;;){
+            ch= (char) System.in.read();
+            do {
+                ignore= (char) System.in.read();
+            } while (ignore!='\n');
+            if (ch=='q'){
+                System.out.println("q Нажата. Выходим.");
+                break;
+            }
+        }
+    }
+}
