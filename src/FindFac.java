a320ce844a24203a7df1be21e95f36edbbeb03b1
CapYossarian <litovka.sergey@gmail.com>
2023-02-14 22:12:52 +0300

    init5

--- src/FindFac.java
+++ src/FindFac.java
@@ -1,11 +1,14 @@
 public class FindFac {
     public static void main(String[] args) {
+        int itcount=0;
         for (int i=2; i<=20; i++ ){
             System.out.print("Делители числа " + i+":");
-            for (int j=2 ; j< i; j++){
+            for (int j=2 ; j< i/2; j++){ //делитель не может превышать величины половина делимого
                 if ((i%j) == 0) System.out.print(j+" ");
+                itcount++;
             }
             System.out.println();
         }
+        System.out.println("Итерпций "+itcount);
     }
 }
