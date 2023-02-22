8e4de87f7d77b3fe9aa789d3932907a8d61596f5
CapYossarian <litovka.sergey@gmail.com>
2023-02-22 15:02:18 +0300

    init 14

---
+++ src/Encode.java
@@ -0,0 +1,9 @@
+public class Encode {
+    public static void main(String[] args) {
+        String originaltext = "";
+        String encodemsg = "";
+        String decodemsg = "";
+        System.out.println("Исходный текст=>"+originaltext);
+
+    }
+}
