7ba36db716e1d5f60e55cfd4ec8d6181ceb9d624
CapYossarian <litovka.sergey@gmail.com>
2023-03-16 17:06:32 +0300

    init 20230316_1706

---
+++ src/ShowFile.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class ShowFile {
+    public static void main(String[] args) {
+        int i;
+        //Проверка входного параметра строки запуска
+        if (args.length!=1){
+            System.out.println("Параметры запуска ShowFile <ИмяФайла>");
+            return;
+        }
+        //try с ресурсами
+        try (FileInputStream fin = new FileInputStream(args[0])){
+            do {
+                i = fin.read();
+                if(i != -1) System.out.print((char) i);
+            } while (i!=-1);
+        } catch (IOException exc){
+            System.out.println("Ошибка ввода-вывода "+exc);
+        }
+
+    }
+}
