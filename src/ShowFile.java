bd6457afc8765aab9ca21845ed058b1421cdaa68
CapYossarian <litovka.sergey@gmail.com>
2023-03-17 15:40:28 +0300

    init 20230317_1540

--- src/ShowFile.java
+++ src/ShowFile.java
@@ -13,6 +13,7 @@ public class ShowFile {
                 i = fin.read();
                 if(i != -1) System.out.print((char) i);
             } while (i!=-1);
+            System.out.println();
         } catch (IOException exc){
             System.out.println("Ошибка ввода-вывода "+exc);
         }
---
+++ src/chardemo/CharDemo.java
@@ -0,0 +1,13 @@
+package chardemo;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char c='ф';
+        int i;
+        System.out.println("ab");
+        System.out.println("аб");
+        System.out.println(c);
+        i=c;
+        System.out.println((char)i);
+    }
+}
